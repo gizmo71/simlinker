52af0a86ffe8564aef3f8215ca5c5392c6e5cbdf
Dave Gymer <gizmo@davegymer.org>
2025-01-01 12:23:52 +0000

    Working!

--- synclinks.ps1
+++ synclinks.ps1
@@ -1,10 +1,30 @@
 # run using: powershell -command "& d:\simlinker\synclinks.ps1"
 
+$root = "D:\simlinker"
+$sourceRoot = [IO.Path]::GetFullPath("$($root)\Sources")
+$targetRoot = [IO.Path]::GetFullPath("$($root)\Targets")
 $sources = @{
-	'Aircraft-2020' = 2020;
-	'Aircraft-2024' = 2024;
-	'Aircraft-Both' = 2020, 2024
+	'*-2020' = 2020;
+	'*-2024' = 2024;
+	'*-Both' = 2020, 2024
 }
-$sources
+
+Get-ChildItem -LiteralPath $targetRoot -Directory -Depth 2 -Attributes "ReparsePoint" -FollowSymlink |
+	Where-Object {$_.Target.StartsWith($sourceRoot)} |
+	ForEach-Object {
+	$_.Delete()
+}
+Write-Host "Previous links removed; now making new links"
+
+foreach ($entry in $sources.GetEnumerator()) {
+	Get-ChildItem -Path $sourceRoot -Filter $entry.Key -Directory -PipelineVariable sourcePath | ForEach-Object {
+		Get-ChildItem -LiteralPath $sourcePath.FullName -PipelineVariable sourceFolder | ForEach-Object {
+			foreach ($targetFolder in $entry.Value) {
+				$targetPath = [IO.Path]::GetFullPath("$($targetRoot)\$($targetFolder)\$($sourceFolder.Name)")
+				New-Item -Path $targetPath -ItemType SymbolicLink -Value ([WildcardPattern]::Escape($sourceFolder.FullName))
+			}
+		}
+	}
+}
 
 Read-Host -Prompt "Press any key to continue"
